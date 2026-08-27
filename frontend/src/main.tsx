340ecae5f9c04fe744599489ff316e2447b995ef
sultaniqalbi <m.nursalmi@gmail.com>
2026-08-27 21:11:36 +0700

    perf: code-split style-admin.css away from homepage bundle to reduce unused CSS, and update CSP font-src for Google Fonts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/style-main.css'
-import './styles/style-admin.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
